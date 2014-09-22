eb4b795b8d39618f12ad7f21e96938dbf23c88e7
rhochmuth <rhochmuth@D071451-VHD1.monroecc.edu>
2014-09-22 16:05:56 -0400

    Updated Lab1 commenting

--- Quadratic/src/com/quadratic/main/Lab1.java
+++ Quadratic/src/com/quadratic/main/Lab1.java
@@ -4,17 +4,22 @@
 package com.quadratic.main;
 
 /**
+ * The "driver" class for this program.
+ * Contains the main method to start the program
+ * and interact with the Quadratic and QuadTest classes.
+ * 
  * @author Jason Griffith & Ryan Hochmuth
  *
  */
-public class Lab1 {
+public class Lab1 
+{
 
 	/**
+	 * The main method to start the program.
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
 	}
-
 }
