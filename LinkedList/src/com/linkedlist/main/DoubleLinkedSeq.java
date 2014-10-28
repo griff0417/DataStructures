3c136524aead740cf889d0575ff7ba158c6fde36
griff0417 <griff0417@gmail.com>
2014-10-28 17:28:15 -0400

    changeElementAtIndex

--- LinkedList/src/com/linkedlist/main/DoubleLinkedSeq.java
+++ LinkedList/src/com/linkedlist/main/DoubleLinkedSeq.java
@@ -434,7 +434,8 @@ public class DoubleLinkedSeq implements Cloneable
 	 */
 	public void elementToCurrent(int index)
 	{
-		if (isCurrent()){
+		if (isCurrent())
+		{
 			DoubleNode tempNode = head.getLink();
 		
 			for (int x = 0; x == index; x++)
@@ -448,6 +449,7 @@ public class DoubleLinkedSeq implements Cloneable
 					throw new IllegalStateException();
 				}
 			}
+			
 			tempNode.setData(cursor.getData());
 		}
 		else
@@ -462,8 +464,24 @@ public class DoubleLinkedSeq implements Cloneable
 	 */
 	public void changeElementAtIndex(int index, double element)
 	{
-		if (index < manyItems)
-			data[index] = element;
+		if (isCurrent())
+		{
+			DoubleNode tempNode = head.getLink();
+		
+			for (int x = 0; x == index; x++)
+			{
+				try
+				{
+					tempNode = tempNode.getLink();
+				}
+				catch (IllegalStateException e)
+				{
+					throw new IllegalStateException();
+				}
+			}
+			
+			tempNode.setData(element);
+		}
 		else
 			throw new IllegalStateException("There is no current element.");
 	}
